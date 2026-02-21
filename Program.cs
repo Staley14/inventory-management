8421a80c69e804f075e3f57f0be9bbc94ba7348a
Staley <staleylane101@gmail.com>
2026-02-21 11:15:48 +0800

    Remove comment citation from Main method

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using InventoryManagement.Views;
+
+namespace InventoryManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            [cite_start]
+            InventoryView view = new InventoryView();
+            view.Run();
+        }
+    }
+}
